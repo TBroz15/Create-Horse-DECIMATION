b9e265a313447027a14b9e1ca5095def3cf7426f
TBroz15 <78770366+TBroz15@users.noreply.github.com>
2026-05-31 14:30:23 +0800

    force put horse.jpg in basin

---
+++ src/main/java/dev/tuxebro/create_horse_decimation/mixin/BasinInventoryMixin.java
@@ -0,0 +1,20 @@
+package dev.tuxebro.create_horse_decimation.mixin;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.simibubi.create.content.processing.basin.BasinInventory;
+import dev.tuxebro.create_horse_decimation.ModItems;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(BasinInventory.class)
+public class BasinInventoryMixin {
+    @WrapOperation(method = "insertItem", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;isSameItemSameComponents(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/item/ItemStack;)Z"))
+    private static boolean forcePutHorseJPGOnBasin(ItemStack stack, ItemStack other, Operation<Boolean> original) {
+        if (stack.is(ModItems.HORSE_JPG))
+            return false; // force insert
+
+        return original.call(stack, other);
+    }
+}
