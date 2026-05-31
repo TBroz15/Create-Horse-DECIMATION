f0c52b5d74c40d7c224dbed79f1f1425c9b6aaff
TBroz15 <78770366+TBroz15@users.noreply.github.com>
2026-05-31 17:04:26 +0800

    actually fix horse sound

--- src/main/java/dev/tuxebro/create_horse_decimation/mixin/MechanicalPressBlockEntityMixin.java
+++ src/main/java/dev/tuxebro/create_horse_decimation/mixin/MechanicalPressBlockEntityMixin.java
@@ -1,44 +1,47 @@
 package dev.tuxebro.create_horse_decimation.mixin;
 
-import com.simibubi.create.content.kinetics.press.MechanicalPressBlockEntity;
-import dev.tuxebro.create_horse_decimation.ModBlocks;
+import com.simibubi.create.content.kinetics.press.PressingBehaviour;
+import com.simibubi.create.content.processing.basin.BasinBlockEntity;
+import dev.tuxebro.create_horse_decimation.ModItems;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
-import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.ArrayList;
-import java.util.Collections;
-
-@Mixin(MechanicalPressBlockEntity.class)
+// one hell of a way to detect horse sounds
+@Mixin(PressingBehaviour.class)
 public class MechanicalPressBlockEntityMixin {
-    @Inject(method = "onItemPressed", at = @At("HEAD"))
-    public void onItemPressed(ItemStack result, CallbackInfo ci) {
-        if (!result.is(ModBlocks.HORSE_BLOCK.asItem())) return;
+    @Inject(method = "applyOnBasin", at = @At(value = "HEAD"))
+    public void makeHorseSoundOnPress(CallbackInfo ci) {
+        var self = (PressingBehaviour) (Object) this;
+        var level = self.blockEntity.getLevel();
+        var pos = self.getPos();
 
-        var self = (MechanicalPressBlockEntity) (Object) this;
+        BlockEntity blockEntity = level.getBlockEntity(pos.below(2));
+        if (blockEntity == null) return;
+        if (!(blockEntity instanceof BasinBlockEntity basin)) return;
 
-        var level = self.getLevel();
-        var pos = self.getBlockPos();
+        var basinInv = basin.getInputInventory();
+        var hasHorseJPG = basinInv.getItem(0).is(ModItems.HORSE_JPG);
+
+        if (!hasHorseJPG) return;
 
         if (!(level instanceof ServerLevel serverLevel)) return;
 
-        new ArrayList<>(Collections.nCopies(4, null)).forEach((item) -> {
-            serverLevel.playSound(
-                    null,
-                    pos.getX(),
-                    pos.getY(),
-                    pos.getZ(),
-                    SoundEvents.HORSE_HURT,
-                    SoundSource.BLOCKS,
-                    1.0f,
-                    1.0f);
-        });
-
-
+        serverLevel.playSound(
+                null,
+                pos.getX(),
+                pos.getY(),
+                pos.getZ(),
+                SoundEvents.HORSE_HURT,
+                SoundSource.BLOCKS,
+                0.5f,
+                1.0f);
     }
 }
+
+
