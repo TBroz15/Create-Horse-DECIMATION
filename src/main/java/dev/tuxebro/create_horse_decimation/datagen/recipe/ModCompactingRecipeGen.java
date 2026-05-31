1c64f0a5660d1aa4ce7f06f931920dce389fb19c
TBroz15 <78770366+TBroz15@users.noreply.github.com>
2026-05-31 13:55:13 +0800

    make horse block craftable only by pressing
    i forgor

--- src/main/java/dev/tuxebro/create_horse_decimation/datagen/recipe/ModCompactingRecipeGen.java
+++ src/main/java/dev/tuxebro/create_horse_decimation/datagen/recipe/ModCompactingRecipeGen.java
@@ -3,6 +3,7 @@ package dev.tuxebro.create_horse_decimation.datagen.recipe;
 import com.simibubi.create.api.data.recipe.CompactingRecipeGen;
 import com.simibubi.create.content.processing.recipe.HeatCondition;
 import dev.tuxebro.create_horse_decimation.CreateHorseDecimation;
+import dev.tuxebro.create_horse_decimation.ModBlocks;
 import dev.tuxebro.create_horse_decimation.ModFluids;
 import dev.tuxebro.create_horse_decimation.ModItems;
 import net.minecraft.core.HolderLookup;
@@ -15,6 +16,18 @@ public class ModCompactingRecipeGen extends CompactingRecipeGen {
             .require(ModFluids.HORSE_GLUE.get(), 100)
             .output(ModItems.HORSE_GLUE_BALL)
             .requiresHeat(HeatCondition.HEATED));
+
+    GeneratedRecipe HORSE_BLOCK = create("horse_block", b -> b
+            .require(ModItems.HORSE_JPG)
+            .require(ModItems.HORSE_JPG)
+            .require(ModItems.HORSE_JPG)
+            .require(ModItems.HORSE_JPG)
+            .require(ModItems.HORSE_JPG)
+            .require(ModItems.HORSE_JPG)
+            .require(ModItems.HORSE_JPG)
+            .require(ModItems.HORSE_JPG)
+            .require(ModItems.HORSE_JPG)
+            .output(ModBlocks.HORSE_BLOCK));
 
     public ModCompactingRecipeGen(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
         super(output, registries, CreateHorseDecimation.MOD_ID);
